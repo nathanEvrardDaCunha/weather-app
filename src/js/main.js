e746cb1aa1cf8ddc3a31ecba43249e0d1e0c208f
EVRARD-DA CUNHA Nathan <nathan.evrarddacunha@proton.me>
2025-02-03 12:41:55 +0100

    feat: fetch weather by city

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,32 @@ const sum = (a, b) => {
 }
 
 export {sum};
+
+const fetchByCity = () => {
+  const api = import.meta.env.VITE_WEATHER_API;
+  const city = 'Paris';
+
+  return new Promise((resolve, reject) => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api}`)
+    .then((response) => {
+      return response.json();
+    })
+    .then((value) => {
+      resolve(value);
+    })
+    .catch((error) => {
+      reject(error);
+    })
+  })
+}
+
+const displayWeather = async () => {
+  try {
+    const result = await fetchByCity();
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+displayWeather();
