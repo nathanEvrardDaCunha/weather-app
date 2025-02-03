539d65fd35e788c304cc658066367bcb9cc908e9
EVRARD-DA CUNHA Nathan <nathan.evrarddacunha@proton.me>
2025-02-03 14:37:55 +0100

    feat: define, format and sanitize city name

--- src/js/main.js
+++ src/js/main.js
@@ -38,6 +38,47 @@ const cityNameInput = document.getElementById('city-name');
 
 // TODO: Remove all console.log
 
+const defineCity = (city) => {
+    const SHORTEST_CITY_NAME_WORLDWIDE = 1; // Sweden city name
+    const LONGUEST_CITY_NAME_WORLDWIDE = 85; // Taumata fullname
+
+    // TODO: Create custom CityName Error to DRY my error message ?
+
+    if (city === null) {
+        throw new Error('> ERROR: City name is null !');
+    }
+
+    if (city === undefined) {
+        throw new Error('> ERROR: City name is undefined !');
+    }
+
+    if (city.length < SHORTEST_CITY_NAME_WORLDWIDE) {
+        throw new Error('> ERROR: City name is too short !');
+    }
+
+    if (city.length > LONGUEST_CITY_NAME_WORLDWIDE) {
+        throw new Error('> ERROR: City name is too long !');
+    }
+
+    if (typeof city !== 'string') {
+        throw new Error('> ERROR: City name is not a string !');
+    }
+};
+
+const formatCity = (city) => {
+    return city.toLowerCase().trim();
+};
+
+const sanitizeCity = (city) => {
+    const regexLetterOnly = /^[A-Za-z]+$/;
+
+    if (!city.match(regexLetterOnly)) {
+        throw new Error(
+            '> ERROR: City contain character that are not letters !'
+        );
+    }
+};
+
 cityFormElement.addEventListener('submit', (event) => {
     event.preventDefault();
 
@@ -45,10 +86,18 @@ cityFormElement.addEventListener('submit', (event) => {
     // TODO: Format data => (Create test ?)
     // TODO: Sanitize data => (Create test ?)
 
-    const city = cityNameInput.value;
+    let city = String(cityNameInput.value);
+
+    try {
+        city = formatCity(city);
+        defineCity(city);
+        sanitizeCity(city);
+    } catch (error) {
+        // TODO: Create and display error message
+        console.log(error.message);
+    }
 
     // TODO: Display data to HTML tags
-    // TODO: Create error message
 
     displayWeather(city);
 });
